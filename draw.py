e3ce76ba84ab6466292deed426d07e94f8bde438
Prasanth S <prasanthshanmugam1402@gmail.com>
2025-09-18 01:00:52 +0100

    Learnt Modules, Imports, Functions

---
+++ draw.py
@@ -0,0 +1,11 @@
+class draw:
+    length = 10
+    width = 5
+
+    def __init__(self):
+        print("Initialised Draw Class with %s %s" %(self.length , self.width))
+
+def drawLine():
+    return "DRAWING LINE FROM DRAW.PY FILE"
+
+d = draw()
---
+++ game.py
@@ -0,0 +1,9 @@
+from draw import drawLine as drawALine
+
+class game:
+
+    def __init__(self):
+        print("Initialised Game class")
+
+game()
+print(drawALine())
